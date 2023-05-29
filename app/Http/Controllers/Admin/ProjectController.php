cdc0a56736f376d921c4465ca44cb9264b55df7c
christiancamilleri <camilleri.christian@outlook.com>
2023-05-29 14:20:39 +0200

    first commit

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * 
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        $types = Type::all();
+
+        return view('admin.projects.index', compact('projects', 'types'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * 
+     */
+    public function create()
+    {
+        $types = Type::all();
+        $technologies = Technology::all();
+        return view('admin.projects.create', compact('types', 'technologies'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $this->validation($request);
+
+        $formData = $request->all();
+
+
+        $newProject = new Project();
+
+        if ($request->hasFile('thumb_preview')) {
+
+            $path = Storage::put('post_images', $request->thumb_preview);
+
+            $formData['thumb_preview'] = $path;
+        }
+
+        $newProject->fill($formData);
+
+
+        $newProject->slug = Str::slug($formData['name'], '-');
+
+        $newProject->save();
+
+        if (array_key_exists('technologies', $formData)) {
+            $newProject->technologies()->attach($formData['technologies']);
+        }
+
+        return redirect()->route('admin.projects.show', $newProject);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     *
+     */
+    public function show(Project $project)
+    {
+        $types = Type::all();
+        return view('admin.projects.show', compact('project', 'types'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * 
+     */
+    public function edit(Project $project)
+    {
+        $types = Type::all();
+        $technologies = Technology::all();
+        return view('admin.projects.edit', compact('project', 'types', 'technologies'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Project $project)
+    {
+        $this->validation($request);
+
+
+        $formData = $request->all();
+
+        if ($request->hasFile('thumb_preview')) {
+
+            if ($project->thumb_preview) {
+
+                Storage::delete($project->thumb_preview);
+            }
+            $path = Storage::put('post_images', $request->thumb_preview);
+
+            $formData['thumb_preview'] = $path;
+        }
+
+
+        $project->update($formData);
+
+        $project->save();
+
+        if (array_key_exists('technologies', $formData)) {
+            $project->technologies()->sync($formData['technologies']);
+        } else {
+            $project->technologies()->detach();
+        }
+
+        return redirect()->route('admin.projects.show', $project);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Project $project)
+    {
+
+        $project->delete();
+
+        return redirect()->route('admin.projects.index');
+    }
+    private function validation($request)
+    {
+
+        $formData = $request->all();
+
+        $validator = FacadesValidator::make($formData, [
+            'name' => 'required',
+            'thumb_preview' => 'nullable|image|max:4096',
+            'description' => 'required',
+            'link_repo' => 'required',
+            'type_id' => 'nullable|exists:types,id',
+        ], [
+            'name.required' => 'Questo campo non può rimanere vuoto',
+            'thumb_preview.max' => "La dimensione del file è troppo grande",
+            'thumb_preview.image' => "Il file deve essere di tipo immagine",
+            'description.required' => 'Questo campo non può rimanere vuoto',
+            'link_repo.required' => 'Questo campo non può rimanere vuoto',
+            'type_id.exists' => 'Il type deve essere presente nel nostro sito',
+
+        ])->validate();
+
+        return $validator;
+    }
+}
---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts/admin')
+
+@section('content')
+<div class="container">
+  <h1>Aggiungi un Progetto</h1>
+
+  <form action="{{route('admin.projects.store')}}"  method="POST" class="py-5" enctype="multipart/form-data">
+    @csrf
+
+    <div class="mb-3">
+      <label for="name">Nome progetto</label>
+      <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" value="{{old('name')}}">
+      @error('name')
+        <div class="invalid-feedback">
+          {{$message}}
+        </div>
+      @enderror
+    </div>
+
+    <div class="mb-3">
+      <label for="thumb_preview">img</label>
+      <input class="form-control @error('thumb_preview') is-invalid @enderror" type="file" name="thumb_preview" id="thumb_preview">
+      @error('thumb_preview')
+        <div class="invalid-feedback">
+          {{$message}}
+        </div>
+      @enderror
+    </div>
+
+    <div class="mb-3">
+      <label for="type_id">Type</label>
+      <select name="type_id" id="type_id" class="form-select @error('type_id') is-invalid @enderror">
+
+        <option value="">Nessuna</option>
+
+        @foreach ($types as $item)
+            <option value="{{$item->id}}" {{$item->id == old('type_id') ? 'selected' : ''}}>{{$item->name}}</option>
+        @endforeach
+
+      </select>
+      @error('type_id')
+        <div class="invalid-feedback">
+          {{$message}}
+        </div>
+      @enderror
+    </div>
+
+      <div class="mb-3 form-group">
+        <h3>Linguaggi utilizzati</h3>
+
+        @foreach($technologies as $item)
+        <div class="form-check">
+          <input type="checkbox" id="technology-{{$item->id}}" name="technologies[]" value="{{$item->id}}">
+          <label for="technology-{{$item->id}}">{{$item->name}}</label>
+        </div>
+        @endforeach
+
+      </div>
+
+    <div class="mb-3">
+      <label for="description">Descrizione progetto</label>
+      <input class="form-control @error('description') is-invalid @enderror" type="text" name="description" id="description"  value="{{old('description')}}">
+      @error('description')
+        <div class="invalid-feedback">
+          {{$message}}
+        </div>
+      @enderror
+    </div>
+
+    <div class="mb-3">
+      <label for="link_repo">Link repository</label>
+      <input class="form-control @error('link_repo') is-invalid @enderror" type="text" name="link_repo" id="link_repo"  value="{{old('link_repo')}}">
+      @error('link_repo')
+        <div class="invalid-feedback">
+          {{$message}}
+        </div>
+      @enderror
+    </div>
+
+
+    <button type="submit" class="btn btn-primary">Aggiungi</button>
+
+  </form>
+
+</div>
+
+@endsection
